ef6b61cd7ad0a828e497d353d28ee92a8f907d57
wangjunhao <snow_line@hotmail.com>
2022-07-14 10:08:43 +0800

    feat(win): Add open browser command

--- win/cmd.go
+++ win/cmd.go
@@ -9,3 +9,7 @@ func OpenExplorer(path string) error {
 	path = strings.ReplaceAll(path, "/", "\\")
 	return exec.Command(`cmd`, `/c`, `explorer`, path).Start()
 }
+
+func OpenBrowser(uri string) error {
+	return exec.Command(`cmd`, `/c`, `start`, uri).Start()
+}
---
+++ win/cmd_test.go
@@ -0,0 +1,7 @@
+package win
+
+import "testing"
+
+func TestOpenBrowser(t *testing.T) {
+	OpenBrowser("www.baidu.com")
+}
